5f0696b0ff76837730062246cf7c24c846da9027
chandan kumar <ck893607@gmail.com>
2022-12-17 20:39:38 +0530

    final done

--- index.js
+++ index.js
@@ -30,4 +30,128 @@ window.onscroll = function(){
       document.querySelector(".nav-top").classList.remove("fixedheader");
     }
   };
-  
+
+        // third slider/corousel //
+
+   document.getElementById(`sec2`).style.display = "none";
+   document.getElementById(`sec3`).style.display = "none";
+   function booknow() {
+     window.location = "/product-page/detail.html";
+   }
+   let count = 1;
+   function changeS() {
+    console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec1`).style.display = "none";
+       document.getElementById(`sec2`).style.display = "block";
+       document.getElementById(`sec3`).style.display = "none";
+       count++;
+     } else if (count === 2) {
+       count++;
+       document.getElementById(`sec1`).style.display = "none";
+       document.getElementById(`sec2`).style.display = "none";
+       document.getElementById(`sec3`).style.display = "block";
+     } else if ((count = 3)) {
+       count = 1;
+       document.getElementById(`sec1`).style.display = "block";
+       document.getElementById(`sec2`).style.display = "none";
+       document.getElementById(`sec3`).style.display = "none";
+     }
+   }
+   function changeS2() {
+     console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec1`).style.display = "none";
+       document.getElementById(`sec2`).style.display = "none";
+       document.getElementById(`sec3`).style.display = "block";
+       count = 3;
+     } else if (count === 2) {
+       count = 1;
+       document.getElementById(`sec1`).style.display = "block";
+       document.getElementById(`sec2`).style.display = "none";
+       document.getElementById(`sec3`).style.display = "none";
+     } else if ((count = 3)) {
+       count = 2;
+       document.getElementById(`sec1`).style.display = "none";
+       document.getElementById(`sec2`).style.display = "block";
+       document.getElementById(`sec3`).style.display = "none";
+     }
+   }
+
+        //second slider//
+
+   document.getElementById(`sec5`).style.display = "none";
+   function booknow() {
+     window.location = "/product-page/detail.html";
+   }
+   function changeS3() {
+    console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec4`).style.display = "none";
+       document.getElementById(`sec5`).style.display = "block";
+       count++;
+     } else if (count === 2) {
+       count=1
+       document.getElementById(`sec5`).style.display = "none";
+       document.getElementById(`sec4`).style.display = "block";
+   }
+  }
+   function changeS4() {
+     console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec5`).style.display = "block";
+       document.getElementById(`sec4`).style.display = "none";
+       count = 2;
+     } else if (count === 2) {
+       count = 1;
+       document.getElementById(`sec4`).style.display = "block";
+       document.getElementById(`sec5`).style.display = "none";
+   }
+  }
+
+         //slider1//
+
+
+  document.getElementById(`sec7`).style.display = "none";
+  document.getElementById(`sec8`).style.display = "none";
+   function booknow() {
+     window.location = "/product-page/detail.html";
+   }
+   function spotlight_plus() {
+    // console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec6`).style.display = "none";
+       document.getElementById(`sec7`).style.display = "block";
+       document.getElementById(`sec8`).style.display = "none";
+       count++;
+     } else if (count === 2) {
+       count++;
+       document.getElementById(`sec6`).style.display = "none";
+       document.getElementById(`sec7`).style.display = "none";
+       document.getElementById(`sec8`).style.display = "block";
+     } else if ((count = 3)) {
+       count = 1;
+       document.getElementById(`sec6`).style.display = "block";
+       document.getElementById(`sec7`).style.display = "none";
+       document.getElementById(`sec8`).style.display = "none";
+     }
+   }
+   function spotlight_minus() {
+    //  console.log(count);
+     if (count === 1) {
+       document.getElementById(`sec6`).style.display = "none";
+       document.getElementById(`sec7`).style.display = "none";
+       document.getElementById(`sec8`).style.display = "block";
+       count = 3;
+     } else if (count === 2) {
+       count = 1;
+       document.getElementById(`sec6`).style.display = "block";
+       document.getElementById(`sec7`).style.display = "none";
+       document.getElementById(`sec8`).style.display = "none";
+     } else if ((count = 3)) {
+       count = 2;
+       document.getElementById(`sec6`).style.display = "none";
+       document.getElementById(`sec7`).style.display = "block";
+       document.getElementById(`sec8`).style.display = "none";
+     }
+   }
