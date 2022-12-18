d92a2c8483ea33cf6bf6b5f5e91c421fcd787321
Darshan <darshannas11@gmail.com>
2022-12-18 12:35:31 +0530

    signup popup added

--- index.js
+++ index.js
@@ -1,157 +1,157 @@
-let i=0;
-let images=[]
+let i = 0;
+let images = []
 
 images[0] = "https://cdn1.tripoto.com/media/filter/nxxl/img/1516992/Image/1670586625_img_0434.jpg"
 images[1] = "https://cdn1.tripoto.com/media/filter/nxxl/img/2215463/Image/1669102212_1667974883_web1.jpeg"
 images[2] = "https://cdn1.tripoto.com/media/filter/nxxl/img/2224454/Image/1670482094_homepage_recovered_op2.jpg"
 
 
-function changeImage(){ 
-   document.querySelector(".container").innerHTML = "";
-   let img = document.createElement("img");
-     img.src = images[i];
- document.querySelector(".container").append(img);
- if(i<images.length-1){
+function changeImage() {
+  document.querySelector(".container").innerHTML = "";
+  let img = document.createElement("img");
+  img.src = images[i];
+  document.querySelector(".container").append(img);
+  if (i < images.length - 1) {
     i++;
-}else{
-    i=0;
+  } else {
+    i = 0;
+  }
+  setTimeout("changeImage()", 3000)
 }
-setTimeout("changeImage()",3000)
-}
-changeImage()   
+changeImage()
 
 
 // When the user scrolls the page, execute myFunction
 
-window.onscroll = function(){
-    if (window.pageYOffset > 80) {
-      document.querySelector(".nav-top").classList.add("fixedheader");
-    } else {
-      document.querySelector(".nav-top").classList.remove("fixedheader");
-    }
-  };
-
-        // third slider/corousel //
-
-   document.getElementById(`sec2`).style.display = "none";
-   document.getElementById(`sec3`).style.display = "none";
-   function booknow() {
-     window.location = "/product-page/detail.html";
-   }
-   let count = 1;
-   function changeS() {
-    console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec1`).style.display = "none";
-       document.getElementById(`sec2`).style.display = "block";
-       document.getElementById(`sec3`).style.display = "none";
-       count++;
-     } else if (count === 2) {
-       count++;
-       document.getElementById(`sec1`).style.display = "none";
-       document.getElementById(`sec2`).style.display = "none";
-       document.getElementById(`sec3`).style.display = "block";
-     } else if ((count = 3)) {
-       count = 1;
-       document.getElementById(`sec1`).style.display = "block";
-       document.getElementById(`sec2`).style.display = "none";
-       document.getElementById(`sec3`).style.display = "none";
-     }
-   }
-   function changeS2() {
-     console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec1`).style.display = "none";
-       document.getElementById(`sec2`).style.display = "none";
-       document.getElementById(`sec3`).style.display = "block";
-       count = 3;
-     } else if (count === 2) {
-       count = 1;
-       document.getElementById(`sec1`).style.display = "block";
-       document.getElementById(`sec2`).style.display = "none";
-       document.getElementById(`sec3`).style.display = "none";
-     } else if ((count = 3)) {
-       count = 2;
-       document.getElementById(`sec1`).style.display = "none";
-       document.getElementById(`sec2`).style.display = "block";
-       document.getElementById(`sec3`).style.display = "none";
-     }
-   }
-
-        //second slider//
-
-   document.getElementById(`sec5`).style.display = "none";
-   function booknow() {
-     window.location = "/product-page/detail.html";
-   }
-   function changeS3() {
-    console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec4`).style.display = "none";
-       document.getElementById(`sec5`).style.display = "block";
-       count++;
-     } else if (count === 2) {
-       count=1
-       document.getElementById(`sec5`).style.display = "none";
-       document.getElementById(`sec4`).style.display = "block";
-   }
+window.onscroll = function () {
+  if (window.pageYOffset > 80) {
+    document.querySelector(".nav-top").classList.add("fixedheader");
+  } else {
+    document.querySelector(".nav-top").classList.remove("fixedheader");
   }
-   function changeS4() {
-     console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec5`).style.display = "block";
-       document.getElementById(`sec4`).style.display = "none";
-       count = 2;
-     } else if (count === 2) {
-       count = 1;
-       document.getElementById(`sec4`).style.display = "block";
-       document.getElementById(`sec5`).style.display = "none";
-   }
+};
+
+// third slider/corousel //
+
+document.getElementById(`sec2`).style.display = "none";
+document.getElementById(`sec3`).style.display = "none";
+function booknow() {
+  window.location = "/product-page/detail.html";
+}
+let count = 1;
+function changeS() {
+  console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec1`).style.display = "none";
+    document.getElementById(`sec2`).style.display = "block";
+    document.getElementById(`sec3`).style.display = "none";
+    count++;
+  } else if (count === 2) {
+    count++;
+    document.getElementById(`sec1`).style.display = "none";
+    document.getElementById(`sec2`).style.display = "none";
+    document.getElementById(`sec3`).style.display = "block";
+  } else if ((count = 3)) {
+    count = 1;
+    document.getElementById(`sec1`).style.display = "block";
+    document.getElementById(`sec2`).style.display = "none";
+    document.getElementById(`sec3`).style.display = "none";
   }
+}
+function changeS2() {
+  console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec1`).style.display = "none";
+    document.getElementById(`sec2`).style.display = "none";
+    document.getElementById(`sec3`).style.display = "block";
+    count = 3;
+  } else if (count === 2) {
+    count = 1;
+    document.getElementById(`sec1`).style.display = "block";
+    document.getElementById(`sec2`).style.display = "none";
+    document.getElementById(`sec3`).style.display = "none";
+  } else if ((count = 3)) {
+    count = 2;
+    document.getElementById(`sec1`).style.display = "none";
+    document.getElementById(`sec2`).style.display = "block";
+    document.getElementById(`sec3`).style.display = "none";
+  }
+}
 
-         //slider1//
+//second slider//
+
+document.getElementById(`sec5`).style.display = "none";
+function booknow() {
+  window.location = "/product-page/detail.html";
+}
+function changeS3() {
+  console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec4`).style.display = "none";
+    document.getElementById(`sec5`).style.display = "block";
+    count++;
+  } else if (count === 2) {
+    count = 1
+    document.getElementById(`sec5`).style.display = "none";
+    document.getElementById(`sec4`).style.display = "block";
+  }
+}
+function changeS4() {
+  console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec5`).style.display = "block";
+    document.getElementById(`sec4`).style.display = "none";
+    count = 2;
+  } else if (count === 2) {
+    count = 1;
+    document.getElementById(`sec4`).style.display = "block";
+    document.getElementById(`sec5`).style.display = "none";
+  }
+}
+
+//slider1//
 
 
-  document.getElementById(`sec7`).style.display = "none";
-  document.getElementById(`sec8`).style.display = "none";
-   function booknow() {
-     window.location = "/product-page/detail.html";
-   }
-   function spotlight_plus() {
-    // console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec6`).style.display = "none";
-       document.getElementById(`sec7`).style.display = "block";
-       document.getElementById(`sec8`).style.display = "none";
-       count++;
-     } else if (count === 2) {
-       count++;
-       document.getElementById(`sec6`).style.display = "none";
-       document.getElementById(`sec7`).style.display = "none";
-       document.getElementById(`sec8`).style.display = "block";
-     } else if ((count = 3)) {
-       count = 1;
-       document.getElementById(`sec6`).style.display = "block";
-       document.getElementById(`sec7`).style.display = "none";
-       document.getElementById(`sec8`).style.display = "none";
-     }
-   }
-   function spotlight_minus() {
-    //  console.log(count);
-     if (count === 1) {
-       document.getElementById(`sec6`).style.display = "none";
-       document.getElementById(`sec7`).style.display = "none";
-       document.getElementById(`sec8`).style.display = "block";
-       count = 3;
-     } else if (count === 2) {
-       count = 1;
-       document.getElementById(`sec6`).style.display = "block";
-       document.getElementById(`sec7`).style.display = "none";
-       document.getElementById(`sec8`).style.display = "none";
-     } else if ((count = 3)) {
-       count = 2;
-       document.getElementById(`sec6`).style.display = "none";
-       document.getElementById(`sec7`).style.display = "block";
-       document.getElementById(`sec8`).style.display = "none";
-     }
-   }
+document.getElementById(`sec7`).style.display = "none";
+document.getElementById(`sec8`).style.display = "none";
+function booknow() {
+  window.location = "/product-page/detail.html";
+}
+function spotlight_plus() {
+  // console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec6`).style.display = "none";
+    document.getElementById(`sec7`).style.display = "block";
+    document.getElementById(`sec8`).style.display = "none";
+    count++;
+  } else if (count === 2) {
+    count++;
+    document.getElementById(`sec6`).style.display = "none";
+    document.getElementById(`sec7`).style.display = "none";
+    document.getElementById(`sec8`).style.display = "block";
+  } else if ((count = 3)) {
+    count = 1;
+    document.getElementById(`sec6`).style.display = "block";
+    document.getElementById(`sec7`).style.display = "none";
+    document.getElementById(`sec8`).style.display = "none";
+  }
+}
+function spotlight_minus() {
+  //  console.log(count);
+  if (count === 1) {
+    document.getElementById(`sec6`).style.display = "none";
+    document.getElementById(`sec7`).style.display = "none";
+    document.getElementById(`sec8`).style.display = "block";
+    count = 3;
+  } else if (count === 2) {
+    count = 1;
+    document.getElementById(`sec6`).style.display = "block";
+    document.getElementById(`sec7`).style.display = "none";
+    document.getElementById(`sec8`).style.display = "none";
+  } else if ((count = 3)) {
+    count = 2;
+    document.getElementById(`sec6`).style.display = "none";
+    document.getElementById(`sec7`).style.display = "block";
+    document.getElementById(`sec8`).style.display = "none";
+  }
+}
---
+++ login-signup.js
@@ -0,0 +1,175 @@
+let url = 'https://636a91d9c07d8f936da2035c.mockapi.io/users';
+
+let login_div = document.querySelector('#login-div');
+let signup_div = document.querySelector('#signup-div');
+
+let login_form = document.querySelector('#login-form');
+let signup_form = document.querySelector('#signup-form');
+
+let login_link = document.querySelector('#login-link');
+let signup_link = document.querySelector('#signup-link');
+
+let signupBtn = document.querySelector('#signup-btn');
+
+let activeUser = JSON.parse(localStorage.getItem('active-user')) || null;
+
+let usermenu = document.querySelector('.usermenu')
+
+let users;
+
+getUsers();
+
+async function getUsers() {
+    let res = await fetch(url);
+    let data = await res.json();
+    users = data;
+    console.log(users)
+}
+
+signupBtn.addEventListener('click', function () {
+    openSignupForm()
+})
+
+login_link.addEventListener('click', function () {
+    openLoginForm();
+})
+
+signup_link.addEventListener('click', function () {
+    openSignupForm()
+})
+
+signup_form.addEventListener('submit', function () {
+    signupFromData();
+})
+
+
+async function signupFromData() {
+    let signupData = {
+        name: signup_form.name.value,
+        email: signup_form.email.value,
+        password: signup_form.password.value,
+        mobile: signup_form.mobile.value
+    };
+    let flag = true;
+    if (signupData.name == "") {
+        alert("Please enter name");
+        flag = false;
+    } else if (signupData.email == "") {
+        alert("Please enter email");
+        flag = false;
+    } else if (signupData.password == "") {
+        alert("Please enter password");
+        flag = false;
+    } else if (signupData.mobile == "") {
+        alert("Please enter mobile no");
+        flag = false;
+    }
+
+    for (let user of users) {
+        if (user.email == signupData.email) {
+            alert("Email already in use");
+            flag = false;
+        }
+    }
+
+    if (flag) {
+        let res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(signupData)
+        })
+
+        alert(`Welcome ${signupData.name} to TravelWaala`);
+        location.reload()
+        signup_div.style.display = 'none';
+    }
+
+
+}
+
+login_form.addEventListener('submit', function () {
+    loginUser();
+})
+
+async function loginUser() {
+    let loginEmail = document.querySelector('#login-email').value;
+    let loginPass = document.querySelector('#login-password').value;
+    let res = await fetch(url);
+    let data = await res.json();
+    let userdata;
+    for (let user of data) {
+        if (user.email == loginEmail) {
+            userdata = user;
+        }
+    }
+    if (userdata) {
+        if (userdata.password == loginPass) {
+            alert(`Welcome Back, ${userdata.name}!`);
+            saveDetailsOfActiveUser(userdata);
+            login_div.style.display='none';
+        } else {
+            alert("Please enter correct password");
+        }
+    } else {
+        alert("Email is not registered");
+    }
+}
+
+
+function saveDetailsOfActiveUser(userdata){
+    localStorage.setItem('active-user', JSON.stringify(userdata))
+    activeUser=userdata;
+    afterLogin()
+}
+
+if(activeUser){
+    afterLogin()
+}
+
+function openSignupForm() {
+    login_div.style.display = 'none';
+    signup_div.style.display = 'block';
+}
+
+function openLoginForm() {
+    login_div.style.display = 'block';
+    signup_div.style.display = 'none';
+}
+
+
+let login_btn = document.querySelector('#logout-btn');
+
+login_btn.addEventListener('click', function(){
+    logOut();
+})
+
+
+
+function logOut(){
+    usermenu.style.display='none';
+    document.querySelector('.signup').style.display='block';
+    document.querySelector('#admin-page-div').style.display='none';
+    localStorage.setItem('active-user', null);
+    activeUser=null;
+}
+
+
+function afterLogin(){
+
+    document.querySelector('#user-profile').innerText=activeUser.name || 'User';
+
+    document.querySelector('.signup').style.display='none';
+
+    usermenu.style.display='block';
+   
+
+    // Check if admin has logged in if yes show admin page tab on navbar
+
+    if(activeUser.email=='admin@travelwaala.com'){
+        document.querySelector('#admin-page-div').style.display='block';
+    } else {
+        document.querySelector('#admin-page-div').style.display='none';
+    }
+}
